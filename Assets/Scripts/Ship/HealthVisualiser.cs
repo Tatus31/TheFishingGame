6bfddb961187bb98d09a21bdc23f5e26b9f92fd5
Tatus31 <kris.zborowski2003@gmail.com>
2025-06-18 19:46:34 +0200

    fixed blood leaking even after ship fixed

--- Assets/Scripts/Ship/HealthVisualiser.cs
+++ Assets/Scripts/Ship/HealthVisualiser.cs
@@ -13,34 +13,52 @@ public class HealthVisualiser : MonoBehaviour
         {
             obj.SetActive(false);
         }
-
         ShipDamage.Instance.OnDamageTaken += ShipDamage_OnDamageTaken;
+        ShipDamage.Instance.OnRestoreDamage += ShipDamage_OnRestoreDamage;
+    }
+
+    private void ShipDamage_OnRestoreDamage(object sender, int healAmount)
+    {
+        int currentHealth = ShipDamage.Instance.GetModifiedStatValue(Stats.Health);
+        int maxHealth = ShipDamage.Instance.GetPermanentSavedStatValue(Stats.Health);
+        float healthRatio = (float)currentHealth / maxHealth;
+
+        if (healthRatio > 1f / 3f && bloodFlowObjs.Count > 2)
+        {
+            bloodFlowObjs[2].SetActive(false);
+        }
+
+        if (healthRatio > 2f / 3f && bloodFlowObjs.Count > 1)
+        {
+            bloodFlowObjs[1].SetActive(false);
+        }
+
+        if (healthRatio >= 1f && bloodFlowObjs.Count > 0)
+        {
+            bloodFlowObjs[0].SetActive(false);
+        }
     }
 
     private void ShipDamage_OnDamageTaken(object sender, int damage)
     {
         int currentHealth = ShipDamage.Instance.GetModifiedStatValue(Stats.Health);
         int maxHealth = ShipDamage.Instance.GetPermanentSavedStatValue(Stats.Health);
-
         float healthRatio = (float)currentHealth / maxHealth;
 
-        if (healthRatio < 1f) 
+        if (healthRatio < 1f)
         {
             if (bloodFlowObjs.Count > 0)
                 bloodFlowObjs[0].SetActive(true);
         }
-
-        if (healthRatio <= 2f / 3f) 
+        if (healthRatio <= 2f / 3f)
         {
             if (bloodFlowObjs.Count > 1)
                 bloodFlowObjs[1].SetActive(true);
         }
-
-        if (healthRatio <= 1f / 3f) 
+        if (healthRatio <= 1f / 3f)
         {
             if (bloodFlowObjs.Count > 2)
                 bloodFlowObjs[2].SetActive(true);
         }
     }
-
 }
