ea8df750ea127925e57485bd4821cf879edadc0f
Tatus31 <kris.zborowski2003@gmail.com>
2025-10-20 16:23:00 +0200

    first UI improvement pass
    changed the UI look of the compass, oxygen and Info popup bars

--- Assets/Scripts/ChangeLocation/RespawnShip.cs
+++ Assets/Scripts/ChangeLocation/RespawnShip.cs
@@ -14,7 +14,7 @@ public class RespawnShip : ShipTransporter
 #if UNITY_EDITOR
             Debug.LogWarning($"there exists a {Instance.name} in the scene already");
 #endif
-            Destroy(gameObject);
+            Destroy(Instance);
         }
 
         Instance = this;
