da2c93b6d6b060cff9fdbc78c1f0cdffda8b667f
Tatus31 <kris.zborowski2003@gmail.com>
2025-11-05 13:06:51 +0100

    fade out overlay fix
    fixed the corruption overlay appearing and never disappearing when going underdeck

--- Assets/Scripts/Ship/ShipCorruptionProtection.cs
+++ Assets/Scripts/Ship/ShipCorruptionProtection.cs
@@ -1,6 +1,6 @@
+using System;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class ShipCorruptionProtection : MonoBehaviour
 {
@@ -13,28 +13,51 @@ public class ShipCorruptionProtection : MonoBehaviour
     private bool _isInside = true;
     private Coroutine _corruptRoutine;
 
+    private ShipTransporter _shipTransporter;
+
+    private void Start()
+    {
+        _shipTransporter = FindObjectOfType<ShipTransporter>();
+
+        if (!_shipTransporter)
+        {
+            Debug.LogWarning("ShipTransporter not found");
+        }
+    }
+
     void Update()
     {
         float distance = Vector3.Distance(playerTransform.position, transform.position);
         bool insideNow = distance <= radius;
         
-        if (!insideNow && _isInside && !IsUnderDeck.isUnderDeck)
+        if (IsUnderDeck.isUnderDeck)
+        {
+            if (!_isInside)
+            {
+                _isInside = true;
+                StopCorruption();
+            }
+
+        }
+        
+        if (!insideNow && _isInside)
         {
             _isInside = false;
             StartCorruption();
         }
 
-        if ((insideNow || IsUnderDeck.isUnderDeck) && !_isInside)
+        if (insideNow && !_isInside)
         {
             _isInside = true;
             StopCorruption();
         }
-
     }
 
     private void StartCorruption()
     {
-        StopCorruption(); 
+        if(IsUnderDeck.isUnderDeck)
+            return;
+        
         CameraOverlay.Instance.TriggerOverlayWithDelay(0f, corruptionTime);
         _corruptRoutine = StartCoroutine(CorruptionTimer());
     }
@@ -48,7 +71,13 @@ public class ShipCorruptionProtection : MonoBehaviour
             _corruptRoutine = null;
         }
 
-        CameraOverlay.Instance.EndOverlayEvent(1f);
+        StartCoroutine(FadeOutAfterFrame());
+    }
+
+    private IEnumerator FadeOutAfterFrame()
+    {
+        yield return null;
+        CameraOverlay.Instance.EndOverlayEvent( 1f);
     }
 
     private IEnumerator CorruptionTimer()
@@ -57,21 +86,27 @@ public class ShipCorruptionProtection : MonoBehaviour
 
         while (timer < corruptionTime)
         {
-            if (_isInside) yield break;
+            if (_isInside) 
+                yield break;
+            
             timer += Time.deltaTime;
+            
             yield return null;
         }
+        
 #if UNITY_EDITOR
         Debug.Log("Player corrupted and returned to ship.");
 #endif
         CameraOverlay.Instance.EndOverlayEvent(1f);
         yield return new WaitForSeconds(1f);
-        FindObjectOfType<ShipTransporter>().MovePlayer(respawnTransform, playerTransform.gameObject);
+        _shipTransporter.MovePlayer(respawnTransform, playerTransform.gameObject);
     }
 
     void OnDrawGizmosSelected()
     {
-        if (!playerTransform) return;
+        if (!playerTransform) 
+            return;
+        
         Gizmos.color = Vector3.Distance(playerTransform.position, transform.position) <= radius ? Color.green : Color.red;
         Gizmos.DrawWireSphere(transform.position, radius);
     }
